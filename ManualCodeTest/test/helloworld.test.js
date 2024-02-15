1adc4f58309f7472c399c1e523dc0289366a8936
CamArndt <camarndt16@gmail.com>
2024-02-14 16:59:59 -0800

    Imported hello world manual test code and setup to new reopsitory

---
+++ ManualCodeTest/test/helloworld.test.js
@@ -0,0 +1,8 @@
+import sayHello from "../utils/helloworld.mjs";
+import assert from 'assert';
+
+it("Test Hello World", ()=>{
+    const hello = sayHello();
+
+    assert.equal(hello, "hello Cameron");
+});
